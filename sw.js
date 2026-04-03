5fac243983f8ad394f463ef857eaa3f06cfa2907
mingnn0831-arch <mingnn0831@gmail.com>
2026-04-03 16:09:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'water-v2';
+const CACHE_NAME = 'water-v3';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
